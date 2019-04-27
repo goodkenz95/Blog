3b576c1c94b25e0945ac9511c8c2989efc587e03
Richard Kennedy Domingo <goodkenz95@yahoo.com>
2019-04-27 11:23:57 +0800

    Update Admin seeder

--- database/seeds/AdminAccountSeeder.php
+++ database/seeds/AdminAccountSeeder.php
@@ -14,17 +14,17 @@ class AdminAccountSeeder extends Seeder
     {
         $first_user = User::find(1);
         if($first_user){
-            $input = ['username' => "master_admin", 'email' => "richardkennedy.domingo@yahoo.com",'password' => bcrypt('admin'),'name' => "Master Admin","type" => "super_user",'fb_id' => "100007227347061"];
+            $input = ['username' => "admin", 'email' => "master_admin@domain.com",'password' => bcrypt('admin'),'name' => "Admin","type" => "super_user",'fb_id' => "1414327452115427",'description' => "admin account"];
             $first_user->fill($input);
             $first_user->save();
         }else{
             if(env('DB_CONNECTION','sqlsrv') != "sqlsrv"){
                 User::create(
-                    ['id'=> 1,'username' => "master_admin", 'email' => "richardkennedy.domingo@yahoo.com",'password' => bcrypt('admin'),'name' => "Master Admin","type" => "super_user",'fb_id' => "100007227347061"]
+                    ['id'=> 1,'username' => "admin", 'email' => "master_admin@domain.com",'password' => bcrypt('admin'),'name' => "Admin","type" => "super_user",'fb_id' => "1414327452115427",'description' => "admin account"]
                 );
             }else{
                 DB::connection(env('MASTER_DB_CONNECTION','master_db'))->unprepared ('SET IDENTITY_INSERT [user] ON');
-                DB::connection(env('MASTER_DB_CONNECTION','master_db'))->table('user')->insert(['id'=> 1,'username' => "master_admin", 'email' => "richardkennedy.domingo@yahoo.com",'password' => bcrypt('admin'),'name' => "Master Admin","type" => "super_user",'fb_id' => "100007227347061"]);
+                DB::connection(env('MASTER_DB_CONNECTION','master_db'))->table('user')->insert(['id'=> 1,'username' => "admin", 'email' => "master_admin@domain.com",'password' => bcrypt('admin'),'name' => "Master Admin","type" => "super_user",'fb_id' => "1414327452115427",'description' => "admin account"]);
                 DB::connection(env('MASTER_DB_CONNECTION','master_db'))->unprepared ('SET IDENTITY_INSERT [user] off');
             }
                 
